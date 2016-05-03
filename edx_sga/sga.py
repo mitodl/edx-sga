d2a47c0ac071b02d7bcf496655c6eafcd322bf4e
Alex McArthur <Alex.McArthur@affiliate.imd.org>
2016-05-03 14:30:40 +0100

    quote filename in download

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -528,7 +528,7 @@ class StaffGradedAssignmentXBlock(XBlock):
             return Response(
                 app_iter=app_iter,
                 content_type=mime_type,
-                content_disposition="attachment; filename=" + filename.encode('utf-8'))
+                content_disposition="attachment; filename=\"" + filename.encode('utf-8') + "\"")
         except IOError:
             if require_staff:
                 return Response(
