823f4204f17119a5230839da43b10bba9891995b
George Schneeloch <gschneel@mit.edu>
2017-12-12 16:35:29 -0500

    Update usage

--- run_sga_tests.sh
+++ run_sga_tests.sh
@@ -14,4 +14,4 @@ cd /edx/app/edxapp/edx-platform
 # the migrations in the test take more than 10 minutes to set up, so we need to keep travis occupied
 (while true; do echo "."; sleep 60; done) &
 
-./manage.py lms --settings=test_docker test edx_sga.tests.integration_tests
+pytest edx_sga/tests/integration_tests.py
