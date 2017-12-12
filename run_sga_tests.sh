fef00482e814400a02ffcbd71fe55cd42de9c7be
George Schneeloch <gschneel@mit.edu>
2017-12-12 16:01:00 -0500

    Prevent timeout

--- run_sga_tests.sh
+++ run_sga_tests.sh
@@ -10,4 +10,8 @@ pip install -e .
 
 cd /edx/app/edxapp/edx-platform
 
+
+# the migrations in the test take more than 10 minutes to set up, so we need to keep travis occupied
+(while true; do echo "."; sleep 60; done) &
+
 ./manage.py lms --settings=test test edx_sga.tests.integration_tests
