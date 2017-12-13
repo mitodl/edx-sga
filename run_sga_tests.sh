1eded52d236948cba42ea1b3fc2687e22ca4f8e7
George Schneeloch <gschneel@mit.edu>
2017-12-13 13:54:23 -0500

    Run install_prereqs

--- run_sga_tests.sh
+++ run_sga_tests.sh
@@ -3,13 +3,14 @@ set -e
 
 source /edx/app/edxapp/venvs/edxapp/bin/activate
 
-cd /edx-sga
+cd /edx/app/edxapp/edx-platform
+NO_PYTHON_UNINSTALL=1 paver install_prereqs
 
+cd /edx-sga
 pip uninstall edx-sga -y
 pip install -e . -r test_requirements.txt
 
 cd /edx/app/edxapp/edx-platform
 mkdir -p reports
 
-cp /edx-sga/edx_sga ./lms/djangoapps/ -r
 pytest lms/djangoapps/edx_sga/tests/integration_tests.py
