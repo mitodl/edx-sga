c139fb8152ddb16daa589f2ddf8a56e9e732a4d4
George Schneeloch <gschneel@mit.edu>
2017-12-13 14:21:02 -0500

    Fix line

--- run_sga_tests.sh
+++ run_sga_tests.sh
@@ -35,4 +35,5 @@ pip install -e . -r test_requirements.txt
 cd /edx/app/edxapp/edx-platform
 mkdir -p reports
 
+cp /edx-sga/edx_sga ./lms/djangoapps/ -r
 pytest lms/djangoapps/edx_sga/tests/integration_tests.py
