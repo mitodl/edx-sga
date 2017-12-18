db2f271d19dae2ab29415eaa314ed221e6de55ff
George Schneeloch <gschneel@mit.edu>
2017-12-18 15:34:44 -0500

    WIP

--- run_devstack_integration_tests.sh
+++ run_devstack_integration_tests.sh
@@ -42,4 +42,5 @@ pip freeze
 cd /edx/app/edxapp/edx-platform
 pytest lms/djangoapps/edx_sga/tests/integration_tests.py --cov-config /edx-sga/.coveragerc --cov ./lms/djangoapps/edx_sga
 coverage xml --rcfile /edx-sga/.coveragerc
+env
 codecov
