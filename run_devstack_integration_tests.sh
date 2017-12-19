09f071d2ff5c0a8c47b02d78a10ba4edad00e4b0
George Schneeloch <gschneel@mit.edu>
2017-12-19 11:58:14 -0500

    Handle missing directory

--- run_devstack_integration_tests.sh
+++ run_devstack_integration_tests.sh
@@ -41,6 +41,7 @@ pip freeze
 # adjust test files for integration tests
 cp /edx/app/edxapp/edx-platform/setup.cfg .
 rm ./pytest.ini
+mkdir test_root  # for edx
 
 pytest ./edx_sga/tests/integration_tests.py --cov .
 coverage xml
