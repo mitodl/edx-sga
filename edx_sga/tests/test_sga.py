b54c86726229f2630bfa660942b7e882e8e4e03a
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2020-12-10 21:15:23 +0500

    Upgrade To Python 3.8

--- edx_sga/tests/test_sga.py
+++ edx_sga/tests/test_sga.py
@@ -632,7 +632,7 @@ class StaffGradedAssignmentMockedTests(TempfileMixin):
             'edx_sga.sga.StaffGradedAssignmentXBlock.get_real_user',
             return_value=self.staff
         ), mock.patch(
-            'edx_sga.utils.default_storage.modified_time',
+            'edx_sga.utils.default_storage.get_modified_time',
             return_value=datetime.datetime.now()
         ):
             response = block.prepare_download_submissions(None)
@@ -675,7 +675,7 @@ class StaffGradedAssignmentMockedTests(TempfileMixin):
         ), mock.patch(
             'edx_sga.sga.StaffGradedAssignmentXBlock.get_real_user', return_value=self.staff
         ), mock.patch(
-            'edx_sga.utils.default_storage.modified_time', return_value=datetime.datetime.now()
+            'edx_sga.utils.default_storage.get_modified_time', return_value=datetime.datetime.now()
         ):
             response = block.prepare_download_submissions(None)
             response_body = json.loads(response.body.decode('utf-8'))
@@ -708,7 +708,7 @@ class StaffGradedAssignmentMockedTests(TempfileMixin):
             'edx_sga.sga.StaffGradedAssignmentXBlock.get_real_user',
             return_value=self.staff
         ), mock.patch(
-            'edx_sga.sga.default_storage.modified_time',
+            'edx_sga.sga.default_storage.get_modified_time',
             return_value=datetime.datetime.now()
         ):
             response = block.prepare_download_submissions(None)
