501bd03ccf681f6d952a21d4f0ac14ce41806302
Shadi Naif <shadinaif@gmail.com>
2022-03-10 18:59:10 +0300

    Fix deprecated modified_time in test files

--- edx_sga/tests/test_sga.py
+++ edx_sga/tests/test_sga.py
@@ -635,7 +635,7 @@ class StaffGradedAssignmentMockedTests(TempfileMixin):
             'edx_sga.sga.StaffGradedAssignmentXBlock.get_real_user',
             return_value=self.staff
         ), mock.patch(
-            'edx_sga.utils.default_storage.modified_time',
+            'edx_sga.utils.default_storage.get_modified_time',
             return_value=datetime.datetime.now()
         ):
             response = block.prepare_download_submissions(None)
@@ -678,7 +678,7 @@ class StaffGradedAssignmentMockedTests(TempfileMixin):
         ), mock.patch(
             'edx_sga.sga.StaffGradedAssignmentXBlock.get_real_user', return_value=self.staff
         ), mock.patch(
-            'edx_sga.utils.default_storage.modified_time', return_value=datetime.datetime.now()
+            'edx_sga.utils.default_storage.get_modified_time', return_value=datetime.datetime.now()
         ):
             response = block.prepare_download_submissions(None)
             response_body = json.loads(response.body.decode('utf-8'))
@@ -711,7 +711,7 @@ class StaffGradedAssignmentMockedTests(TempfileMixin):
             'edx_sga.sga.StaffGradedAssignmentXBlock.get_real_user',
             return_value=self.staff
         ), mock.patch(
-            'edx_sga.sga.default_storage.modified_time',
+            'edx_sga.sga.default_storage.get_modified_time',
             return_value=datetime.datetime.now()
         ):
             response = block.prepare_download_submissions(None)
